d2e17e422b0b26f49b3b7c80f770a700148f7a7a
kmcasingal <113958738+kmcasingal@users.noreply.github.com>
2022-10-11 23:12:33 +0800

    updaaate

---
+++ script.js
@@ -0,0 +1,12 @@
+function menu() {
+  var x = document.getElementsByClassName("header-menu")[0];
+  if (x.style.display == "none" || x.style.display == "") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function scrollUp() {
+  document.body.scrollTop = 0;
+}
