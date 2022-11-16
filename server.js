4fe4e68e4b789457765a45582080f8d6998057e9
kmcasingal <113958738+kmcasingal@users.noreply.github.com>
2022-11-16 22:48:34 +0800

    update!

--- server.js
+++ server.js
@@ -4,4 +4,34 @@ const mongoose = require("mongoose");
 
 const app = express();
 
-mongoose.connect("mongodb://localhost:27017/crud", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost:27017/anonx");
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/public", express.static("public"));
+
+const userSchema = {
+  user: String,
+  post: String,
+  tag: String,
+};
+
+const Post = mongoose.model("post", userSchema);
+
+app.get("/", (req, res) => {
+  Post.find({}, function (err, rows) {
+    console.log(rows)
+
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("index", {
+        posts: rows,
+      });
+    }
+  });
+});
+
+app.listen(3000, function () {
+  console.log("server started on port 3000");
+});
