8736edcbdbb8a42fcc86f07e65447db82378532d
kmcasingal <113958738+kmcasingal@users.noreply.github.com>
2022-11-18 00:07:23 +0800

    updateFunction (crUd)

--- server.js
+++ server.js
@@ -11,16 +11,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/public", express.static("public"));
 
 const userSchema = {
-  user: String,
+  username: String,
   post: String,
   tag: String,
+  isUser: String,
+  anon: String,
 };
 
 const Post = mongoose.model("post", userSchema);
 
 app.get("/", (req, res) => {
   Post.find({}, function (err, rows) {
-    console.log(rows)
+    // console.log(rows)
 
     if (err) {
       console.log(err);
@@ -28,6 +30,80 @@ app.get("/", (req, res) => {
       res.render("index", {
         posts: rows,
       });
+    }
+  });
+});
+
+app.get("/create", (req, res) => {
+  Post.find({}, function (err, rows) {
+    // console.log(rows)
+
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("index", {
+        posts: rows,
+      });
+    }
+  });
+});
+
+app.get("/add", (req, res) => {
+  res.render("createPost", {
+  });
+});
+
+app.post("/save", (req, res) => {
+    const post = new Post({
+      username: "DLSUaccount", // temporaryyyyy username -- wala pang login
+      post: req.body.postText,
+      isUser: "true",
+      anon: req.body.anonToggle,
+      tag: req.body.tag,
+    });
+   // console.log( req.body.anonToggle);
+    post.save( function(err){
+      if(err){
+        console.log(err);
+      } else {
+        res.redirect("/");
+      }
+    });
+});
+
+app.get('/edit/:userId', (req, res) => {
+  let userId = req.params.userId;
+
+  console.log("USER ID 1: " + userId);
+  Post.find( {_id: userId}, function(err, result){
+    if(err){
+      console.log(err);
+    } else {
+      console.log("RESULT" + result[0]);
+      res.render("updatePost", {
+        posts: result[0],
+      });
+    }
+  });
+});
+
+app.post("/update", (req, res) => {
+  const userId = req.body.id;
+  const query = {_id: userId};
+  const postVal = req.body.postText;
+  const isUserVal = "true";
+  const anonVal = req.body.anonToggle;
+  const tagVal = req.body.tag;
+
+  console.log("USER ID 2: " + userId);
+  // ganto dapat
+  // db.posts.updateOne({_id : ObjectId("63763ffa152b951120b2a744")}, {$set: {user: "You", post:"hello i am the main user here xd", tags:["flex", "main"]}})
+  Post.updateOne( query, {post: postVal, isUser: isUserVal, anon: anonVal, tag: tagVal}, function(err, result){
+    if(err){
+      console.log(err);
+    } else {
+      console.log(postVal);
+      res.redirect("/");
     }
   });
 });
